9debe19912de994f4bcb72c9744f29f9485a1801
Shane1o <shane41mag@gmail.com>
2025-08-05 18:03:36 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~0jESUDwK#OXBsAlT4R9Kk8w6OFx6UmM1Mb1grTtfcGalhNZ4hYxk",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "27649566695", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
